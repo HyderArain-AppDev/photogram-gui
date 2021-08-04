b281267b2b6903e5e92decf901d8eb45e9296e8f
hyderarain <harain@chicagobooth.edu>
2021-08-04 17:46:37 +0000

    photo detail

--- app/controllers/photo_controller.rb
+++ app/controllers/photo_controller.rb
@@ -3,6 +3,8 @@ class PhotoController < ApplicationController
     render({:template => "all_photo.html.erb"})
   end
   def detail
+    photo_id = params.fetch("photo_id")
+    @photo = Photo.where({:id => photo_id}).first
     render({:template => "photo_detail.html.erb"})
   end
 end
