fd9309e520b6d5d3a57d7b5c98e4a8d879f7045b
hyderarain <harain@chicagobooth.edu>
2021-08-04 17:36:41 +0000

    user detail complete

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -3,6 +3,8 @@ class UserController < ApplicationController
     render({:template => "all_user.html.erb"})
   end
   def detail
+    @username = params.fetch("username")
+    @user = User.where({:username => @username}).first
     render({:template => "user_detail.html.erb"})
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get("/", {:controller => "user", :action => "all"})
   get("/users", {:controller => "user", :action => "all"})
-  get("/users/:user_id", {:controller => "user", :action => "detail"})
+  get("/users/:username", {:controller => "user", :action => "detail"})
   get("/photos", {:controller => "photo", :action => "all"})
   get("/photos/:photo_id", {:controller => "photo", :action => "detail"})
 
