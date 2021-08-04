928797fb057bfbb53db6f8155553cd5ac6391c12
hyderarain <harain@chicagobooth.edu>
2021-08-04 15:39:12 +0000

    routes framework aded

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
 
+  get("/users", {:controller => "", :action => ""}
+  get("/users/:user_id", {:controller => "", :action => ""}
+  get("/photos", {:controller => "", :action => ""}
+  get("/photos/:photo_id", {:controller => "", :action => ""}
 
 end
